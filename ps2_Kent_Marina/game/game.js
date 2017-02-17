2eeaae3522b36f9f32f320063729ec44472c7c73
Marina Kent <mtkent@hotmail.com>
2017-02-16 20:31:17 -0800

    finished both

--- ps2_Kent_Marina/game/game.js
+++ ps2_Kent_Marina/game/game.js
@@ -1,10 +1,15 @@
+// the array of data
 var peopleArr;
 
+// whether the game is over
+var gameover = true;
+
+// this function will fetch the data from teh json file
 function getData() {
 	// will get the data from the JSON file - data is the type of information it's getting
 	$.getJSON('http://localhost:8000/data.json', function(data) {
        	
-       	// output the data into the table
+       	// output the data into the table - not currently being used, but maybe helpful for future implementation
         var output="<table class='table table-hover'>";
         output += "<tr> " +
         		  "<th> First name </th> " +
@@ -25,11 +30,118 @@ function getData() {
 
 }
 
-function newGame() {
-	console.log("new, " peopleArr);	
+
+// the current person we're looking at
+var count = 0;  
+
+// displays the image on the site
+function showImage(){
+  if (! gameover) {
+    var pic = peopleArr.people[count].image;
+    var image = document.getElementById('myImage');  
+    image.src = "profilePics/" + pic;
+    displayOptions();
+
+    count++;
+
+    // will stop playing once we're done with all people
+    if (count === 15){
+      count = 0;
+      quit();
+    }
+  }
 }
 
-function quit() {
-	console.log("quit");
+// the correct answer to click on in the table
+var rightCell;
 
+// will display name options for the game
+function displayOptions() {
+  var ran =Math.floor(Math.random() * 3);
+  var id = "slot" + ran;
+  rightCell = id;
+  var id1 = "slot" + ((ran + 1) % 3);
+  var id2 = "slot" + ((ran + 2) % 3);
+
+  var ans = document.getElementById(id);
+  var ans1 = document.getElementById(id1);
+  var ans2 = document.getElementById(id2);
+
+  answer = peopleArr.people[count].firstname + " " + peopleArr.people[count].lastname;
+
+  randomPerson1 = (count + 8) % 15;
+  randomPerson2 = (count + 3) % 15;
+  fake1 = peopleArr.people[randomPerson1].firstname + " " + peopleArr.people[randomPerson1].lastname;
+  fake2 = peopleArr.people[randomPerson2].firstname + " " + peopleArr.people[randomPerson2].lastname;
+
+
+  ans.innerHTML = answer;
+  ans1.innerHTML = fake1;
+  ans2.innerHTML = fake2;
+
+// will reset to original after game done
+  if (gameover){
+    document.getElementById("slot0").innerHTML = "Option 1";
+    document.getElementById("slot1").innerHTML = "Option 2";
+    document.getElementById("slot2").innerHTML = "Option 3" ;
+  }
+}
+
+// the scores
+var right = 0;
+var wrong = 0;
+var highscore = 0;
+
+// checks if the right cell was clicked, will tell you if you got the right one or not
+function isRight (cell) {
+  if (!gameover) {
+    if (cell.id === rightCell){
+      right++;
+      document.getElementById("rightscore").innerHTML = "Right: " + right; 
+      alert("Correct, good job!");
+    } else {
+      wrong++;
+      document.getElementById("wrongscore").innerHTML = "Wrong: " + wrong; 
+      var nope = "Sorry, that is incorrect. The right name is: " + document.getElementById(rightCell).innerHTML;
+      alert(nope);
+
+
+    }
+    showImage();
+
+  }
+
+}
+
+// will start a new game
+function newGame() {
+    if (gameover) {
+      gameover = false;
+      count = 0;
+      updateScore();
+  	  showImage();
+  }
+}
+
+// will quit a game 
+function quit() {
+	var image = document.getElementById('myImage');  
+  image.src = "";
+  image.alt = "Game Over! Press New Game to play again!"
+  gameover = true;
+  displayOptions();
+}
+
+// will update the scores on the page, will also alert you to a high score 
+function updateScore() {  
+  if (right > highscore){
+    highscore = right;
+    document.getElementById("highscore").innerHTML = "Highscore: " + highscore; 
+    alert("You're getting better, you beat your high score!")
+  }
+  right = 0;
+  document.getElementById("rightscore").innerHTML = "Right: " + right; 
+
+  wrong = 0;
+  document.getElementById("wrongscore").innerHTML = "Wrong: " + wrong; 
 }
--- ps2_Kent_Marina/validation/js/validate.js
+++ ps2_Kent_Marina/validation/js/validate.js
@@ -1,83 +1,90 @@
-var validateField = function(fieldElem, infoMessage, validateFn) {
 
+// function that will validate multiple elements, sending messages as appropriate 
+var validateField = function(fieldElem, infoMessage, validateFn) {
+  // will add a span if one doesn't already exist
   if(fieldElem.parentElement.childElementCount == 1){
     $('<span></span>').insertAfter(fieldElem);
   } 
 
+  // the span that displays the message
   var theSpan = ($(fieldElem.parentElement.children).first().next());
 
+  // if there is no value, will display the info mesage
   if (fieldElem.value === ""){
-    $(theSpan).text(infoMessage);
     $(theSpan).text(infoMessage).removeClass().addClass("info");
 
-
+  // otherwise will say if correct or not
   } else {
-     if (! validateFn(fieldElem) ) {
-        $(theSpan).text("Error");
-        $(theSpan).text("Error").removeClass().addClass("error");
-        return false;
-      } else {
-        $(theSpan).text("Ok");
-        $(theSpan).text("Ok").removeClass().addClass("ok");
-        return true;
-      }
+    if (! validateFn(fieldElem) ) {
+      $(theSpan).text("Error").removeClass().addClass("error");
+      return false;
+    } else {
+      $(theSpan).text("Ok").removeClass().addClass("ok");
+      return true;
+    }
   }
- 
+
 };
 
+//will check the last two - not text inputs, and will alert if incorrect
 function submitValid (fieldElem, infoMessage, validateFn) {
   if (!validateField(fieldElem, infoMessage, validateFn)) {
     var theSpan = ($(fieldElem.parentElement.children).first().next());
 
     $(theSpan).text("");
-    
+
     alert(infoMessage);
   }
 
 }
 
+// checks username is only alphanumeric
 function isAlphaNum (username, message) {
 	return username.value.match(/^[a-z0-9]+$/i);
 }
 
-
+// checks password is 8 chars and has a number
 function passCheck (password) {
   return (password.value.length > 7 && /\d/.test(password.value));
-   
+
 }
 
+// checks phone number is 10 digits
 function isPhoneNum (number) {
   var num = number.value
-	return (num.match(/\d/g) && num.length === 10);
+  return (num.match(/\d/g) && num.length === 10);
 }
 
-// check for .com/other *************
+// checks email correct format [a-z]@[a-z].(com, gov, edu)
 function isEmail (email) {
   var reg = new RegExp   (/^[a-z]+@[a-z]+\.(com|gov|edu)+$/); 
 
   return reg.test(email.value);
 }
 
+// checks that a radio button is pressed
 function radioCheck(form) {
-    console.log("form " + form );
+  console.log("form " + form );
 
-    var radioButtons = form.radColleges;
-    radioChosen = false;
-    for (var i=0; i<radioButtons.length; i++) {
-     if (radioButtons[i].checked)
-      {
-        radioChosen=true;
-      }
-    }
-
-    return radioChosen;
+  var radioButtons = form.radColleges;
+  radioChosen = false;
+  for (var i=0; i<radioButtons.length; i++) {
+   if (radioButtons[i].checked)
+   {
+    radioChosen=true;
+  }
 }
 
+return radioChosen;
+}
+
+
+// checks at least two checkboxes clicked
 function checkChecks(field) {
   var intCount = 0
   for (var i = 0; i < field.length; i++) {
-	     if (field[i].checked)
-         intCount++; }
+    if (field[i].checked)
+     intCount++; }
 
-  return intCount >2;
-}
+   return intCount >2;
+ }
